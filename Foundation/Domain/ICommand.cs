9868ca8e65acbb427be5bbfaf2f947eca577ce08
Stanislav Kychanov <kychanov_sa@directum.ru>
2022-09-03 22:24:20 +0400

    Добавлены базовые интерфейсы для работы с данными в доменном слое.

---
+++ Foundation/Domain/ICommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GlacialBytes.Foundation.Domain
+{
+  /// <summary>
+  /// Интерфейс команды.
+  /// </summary>
+  /// <typeparam name="TKey">Тип ключей в хранилище.</typeparam>
+  /// <typeparam name="TArgs">Тип аргументов команды.</typeparam>
+  /// <typeparam name="TResult">Тип результата команды.</typeparam>
+  public interface ICommand<TKey, in TArgs, out TResult> : IUnitOfWork<TKey>, IDisposable
+  {
+    /// <summary>
+    /// Выполняет команду.
+    /// </summary>
+    /// <param name="args">Аргументы.</param>
+    /// <returns>Результат выполнения.</returns>
+    TResult Execute(TArgs args);
+  }
+
+  /// <summary>
+  /// Интерфейс команды, выполняемой без резульатат.
+  /// </summary>
+  /// <typeparam name="TKey">Тип ключей в хранилище.</typeparam>
+  /// <typeparam name="TArgs">Тип аргументов команды.</typeparam>
+  public interface ICommand<TKey, in TArgs> : IUnitOfWork<TKey>, IDisposable
+  {
+    /// <summary>
+    /// Выполняет команду.
+    /// </summary>
+    /// <param name="args">Аргументы.</param>
+    void Execute(TArgs args);
+  }
+}
---
+++ Foundation/Domain/IUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GlacialBytes.Foundation.Data;
+
+namespace GlacialBytes.Foundation.Domain
+{
+  /// <summary>
+  /// Интерфейс единицы работы.
+  /// </summary>
+  /// <typeparam name="TKey">Тип ключа в хранилище.</typeparam>
+  public interface IUnitOfWork<TKey> : IDisposable
+  {
+    /// <summary>
+    /// Провайдер данных.
+    /// </summary>
+    IDataProvider<TKey> DataProvider { get; }
+
+    /// <summary>
+    /// Фиксирует выполненную работу.
+    /// </summary>
+    void Commit();
+
+    /// <summary>
+    /// Асинхронно фиксирует выполненную работу.
+    /// </summary>
+    /// <returns>Объект задачи.</returns>
+    Task CommitAsync();
+
+    /// <summary>
+    /// Асинхронно фиксирует выполненную работу.
+    /// </summary>
+    /// <param name="cancellationToken">Токен завершения операции.</param>
+    /// <returns>Объект задачи.</returns>
+    Task CommitAsync(CancellationToken cancellationToken);
+  }
+}
